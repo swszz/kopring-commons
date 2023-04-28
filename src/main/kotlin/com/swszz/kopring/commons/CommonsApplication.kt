38f368675d701c26ee04fa6184e9131e29bb49c3
sungwonkim <sungwonkim@sungwonkimui-MacBookPro.local>
2023-04-28 16:28:24 +0900

    initialize: create project

---
+++ src/main/kotlin/com/swszz/kopring/commons/CommonsApplication.kt
@@ -0,0 +1,11 @@
+package com.swszz.kopring.commons
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommonsApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommonsApplication>(*args)
+}
---
+++ src/test/kotlin/com/swszz/kopring/commons/CommonsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.swszz.kopring.commons
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommonsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
